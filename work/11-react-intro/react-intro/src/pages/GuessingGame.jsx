c6938034bb23e0be8b23b15e4d2ea9500bde6e0b
Yitong Wu <44213937+WUY97@users.noreply.github.com>
2023-04-05 13:29:40 -0700

    guess word debug

--- work/11-react-intro/react-intro/src/pages/GuessingGame.jsx
+++ work/11-react-intro/react-intro/src/pages/GuessingGame.jsx
@@ -7,24 +7,28 @@ export default function GuessingGame({ handleLogout, username }) {
     const [guessMessage, setGuessMessage] = React.useState('');
 
     const secretWord = 'RECAT';
-    const validGuess = /^[a-zA-Z]{6,}$/;
+    const validGuess = /^[a-zA-Z]{5}$/;
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const word = event.target.word.value.toUpperCase();
 
+        // If the input did contain a 5 character word, and it was identical (regardless of case) to the secret word, the page will say "XXX is the secret word!"
         if (word === secretWord) {
-            setGuessMessage('You guessed it!');
+            setGuessMessage(word + ' is the secret word!');
             setCurrentGuess(word);
         } else if (word.length === 0) {
-            setGuessMessage('Please enter a guess.');
-        }else if (word.length < 5) {
-            setGuessMessage(word + ' is too short');
-        } else if (!validGuess.test(word)) {
-            setGuessMessage(word + ' is not a valid guess.');
+            setGuessMessage('Please enter a five-letter word.');
+        } else if (word.length !== 5 || !validGuess.test(word)) {
+            // If the input did not contain a 5 character word, the page will add the message "XXX was not a valid word", where XXX is what was in the input field
+            setGuessMessage(word + ' was not a valid word.');
         } else {
+            // If the input did contain a 5 character word, but it was not the secret word, the page will say "XXX had N letters in common", where XXX is what was in the input field, and N is the number of letters in common with the secret word, using the same concepts from the compare code from section 1
             setGuessMessage(
-                word + ' has ' + compare(secretWord, word) + ' correct letters.'
+                word +
+                    ' had ' +
+                    compare(secretWord, word) +
+                    ' letters in common.'
             );
             setCurrentGuess(word);
         }
@@ -35,12 +39,18 @@ export default function GuessingGame({ handleLogout, username }) {
             <h2>
                 Hi {username},
                 {currentGuess
-                    ? ' your most recent guess is ' + currentGuess.toUpperCase()
-                    : ' start guessing!'}
+                    ? ' your most recent guess is ' +
+                      currentGuess.toUpperCase() +
+                      '.'
+                    : " let's guess a five-letter word!"}
             </h2>
             <form onSubmit={handleSubmit}>
                 <label for='word'>Your new guess:</label>
-                <input type='text' name='word' placeholder='Your guess here' />
+                <input
+                    type='text'
+                    name='word'
+                    placeholder='A five-letter word'
+                />
                 {guessMessage ? <p className='message'>{guessMessage}</p> : ''}
                 <button type='submit'>Try</button>
             </form>

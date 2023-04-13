bcf034e8a91d03f1cbac4daa2660450c49949d07
Yitong Wu <44213937+WUY97@users.noreply.github.com>
2023-04-13 15:38:33 -0700

    server config debug

--- final/server.js
+++ final/server.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-
-const app = express();
+const app = require('./app.js');
 const PORT = 4000;
-
-app.use(cookieParser());
-app.use(express.static('./build'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-require('./backend/routes')(app);
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
